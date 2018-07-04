0a3096c35e59f88734558cf20c94c5e65ae50373
Lucas Gomes <gomeslucas103@gmail.com>
2018-07-04 11:05:23 -0300

    Acrescentado traços

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -10,11 +10,12 @@ namespace projetoTeste
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("----------------------------------");
             Console.WriteLine("Eae Mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("----------------------------------");
 
             Console.ReadLine();
         }
